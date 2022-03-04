75bd60505df1b217987be6ba06339c970cb8ad4c
Tiago Gaspar <tiago.sousasilvagaspar@gmail.com>
2022-03-04 20:16:19 -0300

    finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin.admin) {
+      throw new Error(
+        "you do not have the necessary permission to perform this action!"
+      );
+    }
+
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -6,10 +6,17 @@ interface IRequest {
 }
 
 class ShowUserProfileUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) {
+  }
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userAlredyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlredyExists) {
+      throw new Error("User not found!");
+    }
+
+    return userAlredyExists;
   }
 }
 
